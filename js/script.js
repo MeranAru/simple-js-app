1ce01d9133dfc1f0bb15de1005a1e59a80ee93f2
Meran Arumugam <meran.arumugam@gmail.com>
2023-02-28 18:33:05 -0500

    Update script.js
    Added console.log to see it in the devlopment tools

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 let pokemonList=[{name:"Bulbasaur", height:2.04, type:["Grass, Poison"]}, {name:"Charmander", height:2.00, type:"Fire"}, {name:"Squirle", height:1.08, type:"Water"}];
+
+console.log(pokemonList);
