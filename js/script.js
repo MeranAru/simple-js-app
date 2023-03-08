6fbbdba191d85c956333cb1046cbf97f9b6ba08a
Meran Arumugam <meran.arumugam@gmail.com>
2023-03-08 09:59:23 -0500

    Update script.js
    Added console.log to show on console for inspecting document

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ let pokemonList=[
 /*use document.write to show on the webpage and use console.log to show on the console page*/
 for (let i=0; i < pokemonList.length; i++){
     if(pokemonList[i].height <0.6 && pokemonList[i].height >0.4){
+        console.log(pokemonList[i].name + pokemonList[i].height + " that's a small pokemon! ");
         document.write(pokemonList[i].name + pokemonList[i].height + " that's a small pokemon! ");
     }else if(pokemonList[i].height <0.7){
+        console.log(pokemonList[i].name + pokemonList[i].height + " that's an average size pokemon! ");
         document.write(pokemonList[i].name + pokemonList[i].height + " that's an average size pokemon! ");
     }else {
+        console.log(pokemonList[i].name + pokemonList[i].height + " Wow, that's a big pokemon! ")
         document.write(pokemonList[i].name + pokemonList[i].height + " Wow, that's a big pokemon! ")
     }
 }
