4e8470a055927fb379540e82e8d69d22be3fe785
Meran Arumugam <meran.arumugam@gmail.com>
2023-03-13 13:12:54 -0400

    Update script.js
    Add pokemon list2 plus function and removed foe function as not its not needed

--- js/script.js
+++ js/script.js
@@ -3,18 +3,35 @@ let pokemonList=[
     {name:"Charmander", height:0.6, type:"Fire"}, 
     {name:"Squirle", height:0.5, type:"Water"}
 ];
-/*use document.write to show on the webpage and use console.log to show on the console page*/
-for (let i=0; i < pokemonList.length; i++){
-    if(pokemonList[i].height <0.6 && pokemonList[i].height >0.4){
-        console.log(pokemonList[i].name + pokemonList[i].height + " that's a small pokemon! ");
-        document.write(pokemonList[i].name + pokemonList[i].height + " that's a small pokemon! ");
-    }else if(pokemonList[i].height <0.7){
-        console.log(pokemonList[i].name + pokemonList[i].height + " that's an average size pokemon! ");
-        document.write(pokemonList[i].name + pokemonList[i].height + " that's an average size pokemon! ");
-    }else {
-        console.log(pokemonList[i].name + pokemonList[i].height + " Wow, that's a big pokemon! ")
-        document.write(pokemonList[i].name + pokemonList[i].height + " Wow, that's a big pokemon! ")
+
+let pokemonList2=[
+    {name:"Ivysaur", height:1.0, type:["Grass,Posion"]},
+    {name:"Charmeleon", height:1.1, type:"Fire"},
+    {name:"Wartortle", height:1.0, type:"Water"},
+];
+
+/*add function field to make code*/
+function printArrayDetails(list) {
+    for (let i=0; i < list.length; i++){
+            /*use document.write to show on the webpage and use console.log to show on the console page*/
+            console.log(list[i].name);
+            document.write("<p>" + list[i].name + "</p>");
     }
 }
 
-console.log(pokemonList);
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList2);
+
+function divide(dividend, divisor){
+    if (divisor === 0) {
+        return "You’re trying to divide by zero."
+    }else{
+        let result= dividend / divisor;
+        return result;
+    };
+}
+
+console.log(divide(4, 2));
+console.log(divide(7, 0));
+console.log(divide(1, 4));
+console.log(divide(12, -3));
