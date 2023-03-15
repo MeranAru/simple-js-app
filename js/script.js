bc016acecf168ec648b3ac7d200d27c732aef85b
Meran Arumugam <meran.arumugam@gmail.com>
2023-03-15 12:43:05 -0400

    Update script.js
    Wrapped pokemonList with IIFE.
    Added pokemon repository, removed pokemonList2 and added this with items to pokemonList1.
    Updated the forEach method to cope with changes.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-let pokemonList=[
-    {name:"Bulbasaur", height:0.7, type:["Grass, Poison"]}, 
-    {name:"Charmander", height:0.6, type:"Fire"}, 
-    {name:"Squirle", height:0.5, type:"Water"}
-];
+/*IIFE*/
+let pokemonRepository= (function () { 
+    /*pokemon list*/
+    let pokemonList=[
+        {name:"Bulbasaur", height:0.7, type:["Grass, Poison"]}, 
+        {name:"Charmander", height:0.6, type:"Fire"}, 
+        {name:"Squirle", height:0.5, type:"Water"},
+        {name:"Ivysaur", height:1.0, type:["Grass,Posion"]},
+        {name:"Charmeleon", height:1.1, type:"Fire"},
+        {name:"Wartortle", height:1.0, type:"Water"},
+    ]
 
-let pokemonList2=[
-    {name:"Ivysaur", height:1.0, type:["Grass,Posion"]},
-    {name:"Charmeleon", height:1.1, type:"Fire"},
-    {name:"Wartortle", height:1.0, type:"Water"},
-];
+    return {
+        add:function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList
+        }
+    };
+})();
+
+/*testing return function*/
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:'Charizard'});
+console.log(pokemonRepository.getAll());
+
 
 /*use the for each method to make code cleaner*/
-pokemonList.forEach(function(item){
-            /*use document.write to show on the webpage and use console.log to show on the console page*/
-    console.log(item);
-});
-
-pokemonList2.forEach(function(item){
-    /*use document.write to show on the webpage and use console.log to show on the console page*/
-console.log(item);
+pokemonList.forEach(function (pokemon) {
+    if (pokemon.height >= 1.0) {
+        document.write ("<p>" + pokemon.name + " " + pokemon.height + "Wow, that is a big pokemon" + "</p>");
+    } else if (pokemon.height) {
+        document.write ("<p>" + pokemon.name + " " + pokemon.height)
+    }
 });
 
