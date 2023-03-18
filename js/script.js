d9063ab62c0a9bb64f60e7b53dd64a43c9a934f9
Meran Arumugam <meran.arumugam@gmail.com>
2023-03-17 23:58:24 -0400

    Update script.js
    Updated js to make code more cleaner and corrected errors

--- js/script.js
+++ js/script.js
@@ -1,37 +1,61 @@
 /*IIFE*/
-let pokemonRepository= (function () { 
+var pokemonRepository= (function () { 
     /*pokemon list*/
-    let pokemonList=[
-        {name:"Bulbasaur", height:0.7, type:["Grass, Poison"]}, 
-        {name:"Charmander", height:0.6, type:"Fire"}, 
-        {name:"Squirle", height:0.5, type:"Water"},
-        {name:"Ivysaur", height:1.0, type:["Grass,Posion"]},
-        {name:"Charmeleon", height:1.1, type:"Fire"},
-        {name:"Wartortle", height:1.0, type:"Water"},
-    ]
+    let repository = [
+        {name:"Bulbasaur", height:0.7, types:["Grass, Poison"]}, 
+        {name:"Charmander", height:0.6, types:"Fire"}, 
+        {name:"Squirle", height:0.5, types:"Water"},
+        {name:"Ivysaur", height:1.0, types:["Grass,Posion"]},
+        {name:"Charmeleon", height:1.1, types:"Fire"},
+        {name:"Wartortle", height:1.0, types:"Water"},
+    ];
 
-    return {
-        add:function (pokemon) {
-            pokemonList.push(pokemon);
-        },
-        getAll: function() {
-            return pokemonList
+    function add(pokemon) {
+        if (
+            typeof pokemon === 'object' &&
+            'name' in pokemon &&
+            'height' in pokemon &&
+            'types' in pokemon 
+        ) { 
+            repository.push(pokemon);
+        } else {
+            console.log('pokemon is not correct');
         }
-    };
+    }
+    function getAll() {
+        return repository;
+    }
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('my-class');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem)
+        button.addEventListener('click', function () {
+            console.log(pokemon);
+    });}
+    function showDetails(pokemon) {
+        console.log(pokemon)
+    }
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+        };
 })();
 
 /*testing return function*/
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name:'Charizard'});
+pokemonRepository.add({name:'Charizard', height:1.7, types:'Fire, Flying'});
+
 console.log(pokemonRepository.getAll());
 
 
 /*use the for each method to make code cleaner*/
 pokemonRepository.getAll().forEach(function (pokemon) {
-    if (pokemon.height >= 1.0) {
-        document.write ("<p>" + pokemon.name + " " + pokemon.height + " Wow, that is a big pokemon " + "</p>");
-    } else if (pokemon.height) {
-        document.write ("<p>" + pokemon.name + " " + pokemon.height)
-    }
+    pokemonRepository.addListItem(pokemon)
 });
 
